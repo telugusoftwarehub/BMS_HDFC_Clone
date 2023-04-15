8696099fcd637d72d59e2c3b88156eebef45f7b5
gurumurthy93 <61187567+gurumurthy93@users.noreply.github.com>
2023-04-15 19:30:13 +0530

    carousel changes

--- src/components/carousel/carousel.component.js
+++ src/components/carousel/carousel.component.js
@@ -17,19 +17,16 @@ const CarouselComponent = () => {
           <div>
             <img
               src="https://www.hdfcbank.com/content/api/contentstream-id/723fb80a-2dde-42a3-9793-7ae1be57c87f/dfaf4f95-0ef9-4f49-bfe6-867cf47d6a28/NRI/Home/NRO-account-Netbanking-carousel-banner-A-716x298-%282%29.jpg/1.jpeg"
-              alt="afaf"
             />
           </div>
           <div>
             <img
               src="https://www.hdfcbank.com/content/api/contentstream-id/723fb80a-2dde-42a3-9793-7ae1be57c87f/7bc69495-39c1-462b-9d6e-3ff81b92d054/NRI/Home/NRE-account-Netbanking-carousel-banner-B-716x298-%281%29.jpg"
-              alt="afaf"
             />
           </div>
           <div>
             <img
               src="https://www.hdfcbank.com/content/api/contentstream-id/723fb80a-2dde-42a3-9793-7ae1be57c87f/7bc69495-39c1-462b-9d6e-3ff81b92d054/NRI/Home/NRE-account-Netbanking-carousel-banner-B-716x298-%281%29.jpg"
-              alt="afaf"
             />
           </div>
         </HCarousel>
